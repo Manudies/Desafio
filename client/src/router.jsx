c047db22d748ef4679a8d3715ed1d59eb9a81f7c
Manudies <manudies@gmail.com>
2024-07-02 09:05:34 +0200

    Inicio para integración

--- client/src/router.jsx
+++ client/src/router.jsx
@@ -3,9 +3,10 @@ import { createBrowserRouter, redirect } from "react-router-dom";
 import Register from "./pages/register/Register";
 import Textbox from "./components/textbox/Textbox";
 import ErrorPage from "./pages/ErrorPage";
+import Footer from "./components/footer/footer";
 
 import Root from "./pages/Root";
-
+import "./router.css";
 // import TripsList from "./pages/trips/tripList";
 // import TripsListAdmin from "./pages/trips/tripListAdmin";
 // import UserList from "./pages/User/UserList";
@@ -22,12 +23,15 @@ const router = createBrowserRouter([
         path: "/",
         element: 
         <div>
-          <h1>Hola Silver</h1>
+          <img className="fondo" src="../../public/Captura.png" alt="Foto de fondo" />
         </div>,
       },
       {
         path: "/textbox",
-        element: <Textbox  label="Email" error={"error"} placeholder={"placeholder"}/>,
+        element: <>
+                  <Textbox  label="Email" error={"error"} placeholder={"placeholder"}/>
+                  <Footer/>
+        </>,
       },
       {
         path: "/register",
