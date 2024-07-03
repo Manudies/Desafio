13f33cfabcdcd76e4dad82111d624223cb6ee75e
onluani <anitsitska@gmail.com>
2024-07-03 15:53:49 +0200

    styled footer, falta responsive y position:absolute

--- client/src/components/footer/footer.jsx
+++ client/src/components/footer/footer.jsx
@@ -1,77 +1,62 @@
-import PropTypes from "prop-types";
-import "./footer.css";
+import React from 'react';
+import './footer.css';
+import { FiYoutube } from "react-icons/fi";
+import { AiOutlineFacebook } from "react-icons/ai";
+import { AiOutlineWhatsApp } from "react-icons/ai";
+import { FaInstagram } from "react-icons/fa";
+import { CiMail } from "react-icons/ci";
+import { AiOutlinePhone } from "react-icons/ai";
 
-const Footer = ({ className = "" }) => {
+
+const Footer = () => {
   return (
-    <footer className={`footer ${className}`}>
+    <footer className="footer">
       <div className="footer-content">
-        <div className="footer-top">
-          <div className="contact">
-            <b className="text">Contacta</b>
-            <div className="contact-links">
-              <div className="link">
-                <img
-                  className="svg-icon"
-                  loading="lazy"
-                  alt=""
-                  src="/svg.svg"
-                />
-                <div className="infosenioritycom">info@seniority.com</div>
-              </div>
-              <div className="link1">
-                <img className="svg-icon1" alt="" src="/svg-1.svg" />
-                <div className="div">+34 603 180 484</div>
-              </div>
-            </div>
+        
+        <div className="footer-left">
+          <div className="footer-logo">
+            <img src="./seniority_logoblanco_sinfondo_uso_en_fondo_oscuro.png" alt="logo" />
+          </div>
+          <div className="policy-text">
+            <p className="footer-text">© Seniority 2024</p>
+            <a href="#condiciones" className="footer-text-link"><p>Condiciones de Uso</p></a>
+            <a href="#privacidad" className="footer-text-link"><p>Política de Privacidad</p></a>
+            <a href="#cookies" className="footer-text-link"><p>Política de Cookies</p></a>
           </div>
         </div>
-        <div className="social-media-container">
-          <img
-            className="youtube-icon"
-            loading="lazy"
-            alt=""
-            src="/youtube.svg"
-          />
-          <div className="social-media">
-            <img
-              className="social-placeholder-icon"
-              loading="lazy"
-              alt=""
-              src="/frame-35.svg"
-            />
+
+        <div className="footer-middle">
+          <div className="footer-icons">
+            <a href="https://www.youtube.com" className='icon-link' target='_blank' rel='noopener noreferrer'>
+              <FiYoutube className='footer-icon' />
+            </a>
+            <a href="https://www.facebook.com" className='icon-link' target='_blank' rel='noopener noreferrer'>
+              <AiOutlineFacebook className='footer-icon' />
+            </a>
+            <a href="https://www.whatsapp.com" className='icon-link' target='_blank' rel='noopener noreferrer'>
+              <AiOutlineWhatsApp className='footer-icon' />
+            </a>
+            <a href="https://www.instagram.com" className='icon-link' target='_blank' rel='noopener noreferrer'>
+              <FaInstagram className="footer-icon" />
+            </a>
           </div>
-          <img
-            className="social-icons"
-            loading="lazy"
-            alt=""
-            src="/frame.svg"
-          />
-          <img
-            className="facebook-icon"
-            loading="lazy"
-            alt=""
-            src="/facebook.svg"
-          />
         </div>
+
+        <div className="footer-right">
+          <div className="contact-text">
+            <p className='heading'>Contacta</p>
+            <p><CiMail className='contact-icon' /> info@seniority.com</p>
+            <p><AiOutlinePhone className='contact-icon' /> +34 603 180 484</p>
+          </div>
+          </div>
+
+        <div className='enisa-contaner'>
+            <img src="./Sello_Enisa_Horizontal_Color-removebg.png" alt="enisa-logo" className='enisa-png'/>
+          </div>
+
       </div>
-      <div className="seniority-2024-parent">
-        <div className="seniority-2024">© Seniority 2024</div>
-        <div className="link2">Condiciones de Uso</div>
-        <div className="link3">Política de privacidad</div>
-        <div className="link4">Política de cookies</div>
-      </div>
-      <img
-        className="footer-spacing-icon"
-        loading="lazy"
-        alt=""
-        src="/frame-31@2x.png"
-      />
     </footer>
   );
-};
-
-Footer.propTypes = {
-  className: PropTypes.string,
 };
 
 export default Footer;
