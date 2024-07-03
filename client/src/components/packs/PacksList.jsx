d38e5f7ff8f4cedb7fc1d3aa8c8a8412665bc3e5
bego <begofv13@gmail.com>
2024-07-03 17:03:28 +0200

    fix: mostrar info packs

--- client/src/components/packs/PacksList.jsx
+++ client/src/components/packs/PacksList.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useLoaderData } from "react-router-dom"
 import PackCard from "../packCard/packCard";
 import PackCardCompleto from "../packCard/packCardCompleto";
@@ -5,17 +6,24 @@ import "./packList.css";
 
 const packList = () => {
     const packs = useLoaderData();
+    const [selectedPack, setSelectedPack] = useState(null);
+
     console.log("packs",packs)
+
+    const handlePackClick = (pack) => {
+        setSelectedPack(pack);
+    };
+
     return (
         <div className="packsMain">
             <h1>{packs.packName}</h1>
             <section className="packs_container">
                 {packs.map((pack) => (
-                    <PackCard key={pack._id} pack={pack} />
-                ))}
+                    <PackCard key={pack._id} pack={pack} onClick={() => handlePackClick(pack)} />
+                    ))}
             </section>
             <section className="packs_info">
-                <PackCardCompleto pack={packs[1]} />
+            {selectedPack && <PackCardCompleto pack={selectedPack}  />}
             </section>
 
         </div>
