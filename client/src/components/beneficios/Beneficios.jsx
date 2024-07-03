11623f33ff88d30a28e59102aeb71f650bceffcc
bego <begofv13@gmail.com>
2024-07-03 12:50:18 +0200

    feat: pagina consultoria: componentes carrusel,beneficios y herramientas

---
+++ client/src/components/beneficios/Beneficios.jsx
@@ -0,0 +1,30 @@
+import './Beneficios.css';
+
+
+const Beneficios = () => {
+
+
+  return (
+    <main className="beneficiosContainer">
+        <h2>Beneficios</h2>
+        <section className="cardContainer">
+            <div className='cardBeneficios'>
+                <h2>Beneficio 1</h2>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos praesentium voluptas soluta in id! Dolore, praesentium magni ullam cupiditate ad neque illum dignissimos qui tempore ex recusandae iusto, officiis accusamus!</p>
+            </div>
+
+            <div className='cardBeneficios'>
+                <h2>Beneficio 2</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium facere consequatur exercitationem ratione nobis dolores temporibus minus incidunt, dolorem quo harum id sunt accusamus eius dolore quaerat libero provident labore.</p>      
+            </div>
+
+            <div className='cardBeneficios'>
+                <h2>Beneficio 3</h2>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Et laudantium rerum adipisci blanditiis iste ad ipsam maiores fugit neque architecto dolores excepturi, commodi libero ipsa doloremque quod cum, quam velit.</p>
+            </div>
+        </section>
+    </main>
+  );
+}
+
+export default Beneficios;
