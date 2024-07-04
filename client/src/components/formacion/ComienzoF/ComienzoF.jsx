5332b2bd90211c908ba6318cc7b724f770f0f829
bego <begofv13@gmail.com>
2024-07-04 12:15:05 +0200

    feat: metidos los componentes en la pagina formacion

---
+++ client/src/components/formacion/ComienzoF/ComienzoF.jsx
@@ -0,0 +1,21 @@
+import ActionButton from '../../actionButton/actionButton';
+import './ComienzoF.css';
+
+
+const ComienzoF = () => {
+
+
+  return (
+    <div className="comienzoFContainer">
+        <div className='comienzoFTexto'>
+            <h2>dfdsffffffffffffffffo</h2>
+            <h3>¡Desasdfsafdsfsdf!</h3>
+        </div>
+        <div className='botonComienzoF'>
+            <ActionButton label="Descúbrelo Ahora" className={"comienzoButtonF"}/>
+        </div>
+    </div>
+  );
+}
+
+export default ComienzoF;
