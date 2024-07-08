9ac04cb3ceb6cd2225c145c311d922df86cae508
onluani <anitsitska@gmail.com>
2024-07-08 13:29:51 +0200

    scroll to top button

---
+++ client/src/components/scrollButton/ScrollButton.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './ScrollButton.css';
+
+const ScrollButton = () => {
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
+  return (
+    <div className="scroll-button" onClick={scrollToTop}>
+      BACK TO TOP
+    </div>
+  );
+};
+
+export default ScrollButton;
