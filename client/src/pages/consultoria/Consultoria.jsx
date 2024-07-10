06a31cb1934834adf3d57d669ab4520ba31a7383
Manudies <manudies@gmail.com>
2024-07-10 17:35:46 +0200

    feat: Se muestra el Pack que te recomienda DATA

--- client/src/pages/consultoria/Consultoria.jsx
+++ client/src/pages/consultoria/Consultoria.jsx
@@ -11,17 +11,17 @@ import Descubre from "../../components/consultoria/descubre/Descubre";
 import Comienzo from "../../components/consultoria/comienzo/Comienzo";
 import ScrollButton from "../../components/scrollButton/ScrollButton";
 import Punto from "../../components/puntoAcesibilidad/Punto";
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 const consultoria = () => {
-  const [defaultPack, setDefaultPack] = useState(1);
+  const [defaultPack, setDefaultPack] = useState(0);
   const location = useLocation();
   const packsMainRef = useRef(null);
 
   useEffect(() => {
     console.log("location.state", location.state);
-    if (location.state && location.state.pack) {
-      setDefaultPack(location.state.pack);
+    if (location.state && location.state.packId) {
+      setDefaultPack(location.state.packId);
     }
     if (location.state && location.state.scrollTo === "packsMain") {
       packsMainRef.current?.scrollIntoView({ behavior: 'smooth' });
